4ac664e4efd36416c98d7938a96743c4a73a1f84
Jerry Jia <jerry.jia295@gmail.com>
2025-02-16 11:56:12 -0500

    display cards

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,58 +1,147 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, ScrollView, Text, Alert, Button } from 'react-native';
+import React, {useEffect} from 'react';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { LinearGradient } from "expo-linear-gradient";
+import { DisplayCard } from '@/components/displayCard';
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image 
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">My Deck</ThemedText>
-      </ThemedView>
+  const [cardData, setCardData] = React.useState({});
+  useEffect(() => {
 
+  setCardData({ title: 'hi', 
+    description: 'descasdaisgfaigfuaysgdyuasgdiuasdgsayudfauysfdiuafsd', 
+    health: 5, 
+    imgString: "placeholder", 
+    cost: '4', 
+    move1: { cost: 3, description: 'move1asdasdabdsjhasdhavsjdhvasjdvajdshvajhsdvajhvsdajvdsaj', effect: 6, title: 'move1name', type: 'damage' }, 
+    move2: { cost: 4, description: 'move2', effect: 8, title: 'move2name', type: 'damage' }, 
+    owner: 'owner',
+    position: 'n/a' });
+  }, []);
+    
+  return (
+    <LinearGradient colors={['#90ee90', '#e6ffe6']} style={styles.background}>
+    <ScrollView style={styles.screen}>
       
-        <Image 
-          source={require('@/assets/images/card_template.jpg')}
-          style={styles.home_card}
-        />
-      
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Group Cards</ThemedText>
+          <Text>
+          {'\n'}
+          {'\n'}
+          </Text>
+ 
+        <ThemedText type="title" style={styles.titleContainer}>My Deck</ThemedText>
+
+
         
-      </ThemedView>
-    </ParallaxScrollView>
+        {Object.keys(cardData).length && <DisplayCard currentCard={cardData} cardWidth={200}/>}
+        
+
+        <Button title="Change Deck" onPress= {() => {Alert.alert('hi')}} />
+      
+      <ThemedText type="title" style={styles.titleContainer}>Group Cards</ThemedText>
+      
+    </ScrollView>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
+  
   home_card: {
     width: 71.698,
     height: 100,
     borderRadius: 10,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+  
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    fontFamily: 'Chalkboard SE',
+color: '#636363',
+},
+background: {
+    flex: 1,
+
+},
+screen: {
+    flex: 1,
+
+    marginBottom: 80,
+
+},
+container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+},
+header: {
+    fontSize: 20,
+    marginBottom: 16,
+},
+button: {
+    backgroundColor: "#2D923C",
+    padding: 10,
+    borderRadius: 8,
+    marginBottom: 16,
+    shadowColor: "#000000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.4,
+    shadowRadius: 4,
+    elevation: 5,
+},
+buttonText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "bold",
+    
+},
+imageContainer: {
+    borderRadius: 8,
+    marginBottom: 16,
+    shadowColor: "#000000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.4,
+    shadowRadius: 4,
+    elevation: 5,
+},
+image: {
+    width: 158,
+    height: 117,
+    borderRadius: 8,
+},
+errorText: {
+    color: "red",
+    marginTop: 16,
+},
+titleContainer: {
+flexDirection: 'row',
+alignItems: 'center',
+textAlign: 'center',
+gap: 8,
+fontFamily: 'Chalkboard SE',
+color: '#',
+},
+stepContainer: {
+gap: 8,
+marginBottom: 8,
+},
+reactLogo: {
+height: 178,
+width: 290,
+bottom: 0,
+left: 0,
+position: 'absolute',
+},
+input: {
+height: 40,
+margin: 12,
+borderWidth: 1,
+borderRadius: 8,
+fontFamily: 'Chalkboard SE',
+
+}
 });
--- components/displayCard.tsx
+++ components/displayCard.tsx
@@ -1,14 +1,59 @@
-import { useEffect } from 'react';
-import { Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View, Image, StyleSheet, ScrollView, ImageBackground } from 'react-native';
+import { card, CardStorage } from '@/components/card';
 
-export function DisplayCard(currentCard: any) { // Specify the type of currentCard
-    useEffect(() => {
-        console.log(currentCard['name'], 'aaaa');
-    }, []);
+export function DisplayCard({currentCard={}, cardWidth}: any) { 
+  let cardHeight=cardWidth*1.48528012
     return (
-      <Text>
-        {currentCard['name']}
-      </Text>
+      
+      <ImageBackground
+      source = {require('../assets/images/card_template.jpg')}
+      style={{
+        width: cardWidth,
+        height: cardHeight,
+        borderRadius: 10
+      }}>
+        
+        
+        
+        
+
+        <View style={{flexDirection: 'row'}}>
+
+        <Text style={{marginLeft: cardWidth/21, marginTop: cardHeight/60, fontFamily: 'Arial', fontSize: cardWidth/12, color: 'black'}}>
+          {currentCard.title}
+        </Text>
+        <Text style={{flex: 1, marginRight: cardWidth/21, marginTop: cardHeight/35, textAlign:'right', fontFamily: 'Arial', fontSize: cardWidth/17, color: 'black'}}>
+          Health: {currentCard.health}
+        </Text>
+
+        </View>
+
+        <Text style={{marginLeft: cardWidth/20, marginRight: cardWidth/20, marginTop: cardHeight/1.78, fontFamily: 'Arial', fontSize: cardWidth/20, color: 'black'}}>
+          {currentCard.description}
+        </Text>
+
+
+
+        <View style={{flexDirection: 'row'}}>
+        <Text style={{marginLeft: cardWidth/20, marginTop: cardHeight/60, fontFamily: 'Arial', fontSize: cardWidth/15, color: 'black'}}>
+          {currentCard.move1['title']}
+        </Text>
+        <Text style={{marginRight: cardWidth/20,textAlign:'right',flex:1, marginTop: cardHeight/60, fontFamily: 'Arial', fontSize: cardWidth/15, color: 'black'}}>
+          {currentCard.move1['effect']}  {currentCard.move1['cost']}
+        </Text>
+        </View>
+        
+
+
+        <Text style={{marginLeft: cardWidth/20, marginRight: cardWidth/20, marginTop: 0, fontFamily: 'Arial', fontSize: cardWidth/20, color: 'black'}}>
+          {currentCard.move1['description']}
+        </Text>
+        
+      
+      
+    
+      </ImageBackground>
     );
-}
+  }
   
