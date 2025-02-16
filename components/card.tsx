e4e567bc6e773704e70c302966b319d10378749c
suoeh <dylanydai@gmail.com>
2025-02-16 18:55:30 -0500

    Fixed car_screen
    - almost done

--- components/card.tsx
+++ components/card.tsx
@@ -4,6 +4,8 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 import { ListRenderItem } from 'react-native';
 
+export async function getCardsFromGroup(group_id: string) {
+    const docRef = doc(db, 'allGroups', group_id);
     const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
         return docSnap.data()['cards'];
@@ -12,11 +14,27 @@ import { ListRenderItem } from 'react-native';
     }
   }
 
+export async function updateCardData(card_name: string, newCardData: any) {
+    console.log('updating card data in firebase so far...', newCardData);
+    try {
+        const docRef = doc(db, 'allGroups', 'PqS0D76ildkvGidUCPpz');
+        const docSnap = await getDoc(docRef);
+        const currentCards = docSnap.data()?.cards;
+        console.log(currentCards, 'cardRef');
+        currentCards[card_name] = newCardData;
+        console.log('updating card data in firebase', newCardData);
+        await setDoc(docRef, currentCards);
+    } catch (error) {
+        console.log('Error updating card data in Firebase:', error);
+    }
+}
+
 export function CardStorage() {
     let [cardData, setCardData] = useState([]);
 
 	useEffect(() => {
 		const fetchCards = async () => {
+			let storedCards = await getCardsFromGroup('PqS0D76ildkvGidUCPpz');
 			setCardData(storedCards);
 		};
 
@@ -25,6 +43,8 @@ export function CardStorage() {
 
     return {
         // getter
+        getCardData: (key: any) => {
+            return key === 'all' ? cardData : cardData[key] || null;
         },
     }
 }
