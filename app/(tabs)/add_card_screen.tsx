ca0df36346b62782eb5555ed055b85970b02f1ac
Song Eric Yu Li <ericli3690@gmail.com>
2025-02-16 22:01:56 -0500

    quick add card screen fix

--- app/(tabs)/add_card_screen.tsx
+++ app/(tabs)/add_card_screen.tsx
@@ -199,6 +199,7 @@ export default function AddCardScreen() {
             onChangeMoveDesc("");
             onChangeMovePower(0);
             onChangeMoveCost(0);
+            onChangeMoveType("");
         }}
 
     />
